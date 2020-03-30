5ffdb8367e1aa9499fdcc962889dc2fbdd8b9540
yasminGhazy <jesygzaki@gmail.com>
2020-03-30 22:48:19 +0200

    all

---
+++ e-commerce/Scripts/javascript.js
@@ -0,0 +1,24 @@
+﻿$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+
+    navText: [
+        '<i class="fa fa-fw fa-chevron-left" ></i>',
+        '<i class="fa fa-fw fa-chevron-right" ></i>'
+    ],
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        700: {
+            items: 3
+        },
+        1020: {
+            items: 4
+        }
+    }
+
+
+});
