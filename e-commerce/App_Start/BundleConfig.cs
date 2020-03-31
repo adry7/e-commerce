9ba4da8ec2881254f54290a01d66cb1b6e84917d
adry7 <mustafa.adry2025@gmail.com>
2020-03-31 11:47:05 +0200

    -libs update -layout fixes -bundel fixes

--- e-commerce/App_Start/BundleConfig.cs
+++ e-commerce/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace e_commerce
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+                      "~/Scripts/javascript.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,7 +27,9 @@ namespace e_commerce
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                   //"~/Content/site.css",
+                    "~/Content/home.css",
+                    "~/Content/font-awesome.css"));
         }
     }
 }
